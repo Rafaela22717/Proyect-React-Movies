ad77a326b71269ccbb825b3aded1623ae73e1bf8
Rafaela <rafaelaru22@hotmail.com>
2022-03-14 15:05:48 -0300

    components-footer

---
+++ src/components/Footer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+function Footer() {
+
+return(
+
+<footer>
+  <nav>
+    <ul>
+        <li><a href="http://instagram.com" rel="noopener noreferrer" >Instagram</a></li>
+        
+    </ul>
+  </nav>
+   <p>Copyright Alkemy</p>
+</footer>
+
+
+
+
+)
+}
+
+export default Footer;
