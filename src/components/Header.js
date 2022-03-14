f71204f9a5323bb8ebabdea14b2a2c8ec632ab35
Rafaela <rafaelaru22@hotmail.com>
2022-03-14 20:59:42 -0300

    component-Header-modification

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,21 +7,9 @@ function Header() {
     return (
 
     <header>
-       <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-          <Link to="/listado">Listado</Link>
-             
-          </li>
-          <li>
-          <Link to="/contacto">Contacto</Link>
-          </li>
+     
 
-        </ul>
-       </nav>
+     
     </header>
     )
 
