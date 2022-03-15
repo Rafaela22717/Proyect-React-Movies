357fd3449e41cfbbd6e3c33c2ff74f9f960f7e1e
Rafaela <rafaelaru22@hotmail.com>
2022-03-14 21:02:50 -0300

    component-Listado-rendering

--- src/components/Listado.js
+++ src/components/Listado.js
@@ -1,15 +1,29 @@
 import React from 'react';
- 
+import {useEffect, useState} from "react" 
 import { Link, Redirect} from "react-router-dom";
-
+import axios from "axios";
 
 
 function Listado() {
  
     const token = localStorage.getItem('token');//levanto token de localstorage,puede ser un string o nulo
+    
+    const [moviesList, setMoviesList]=useState([]);
 
 
 
+
+   useEffect(()=> {
+     const endpoint="https://api.themoviedb.org/3/discover/movie?api_key=e0cd83b465cb791c8444c563ec079dbf&language=es-ES&page=1"
+    axios.get(endpoint)
+    .then(response => {
+      const apiData=response.data;//paso esta info a un estado de react useState
+      console.log(apiData);
+      setMoviesList(apiData.results);
+    })
+
+   },[setMoviesList]); 
+   console.log(moviesList);   
 
 
 return (//hago una fila con row es bootstrap /y en linea 23 por c/fila columna de bootstrap   
@@ -18,16 +32,23 @@ return (//hago una fila con row es bootstrap /y en linea 23 por c/fila columna d
 
     <div className="row">
         {/*estructura base*/}
-      <div className="col-3" >
-       <div className="card ">
-         <img src="..." className="card-img-top" alt="..."/>
-         <div className="card-body">
-         <h5 className="card-title">Movie Tittle</h5>
-         <p className="card-text">Review Movie</p>
-         <Link href="#" className="btn btn-primary">View Detail</Link>
-     </div>
-    </div>
-   </div>
+       {moviesList.map((movie, idx) => {
+        return (
+          <div className="col-3" key= {idx}>
+          <div className="card my-3 ">
+            <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} className="card-img-top" alt="..."/>
+            <div className="card-body">
+            <h5 className="card-title">{movie.title.substring(0,40)}...</h5>
+            <p className="card-text">{movie.overview.substring(0,100)}... </p>
+            <Link to="#" className="btn btn-primary">View Detail</Link>
+        </div>
+       </div>
+      </div>
+        )
+       })
+      }
+
+     
    
  </div>
  </>
