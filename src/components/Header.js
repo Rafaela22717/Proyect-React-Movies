b1e36ee53ea5b5f289165677a34cd2dbad142baa
Rafaela <rafaelaru22@hotmail.com>
2022-05-20 13:34:57 -0300

    Header component finished

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,42 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import Search from "./Search";
 
-function Header() {
 
+function Header(props) {
 
     return (
+        <header>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark ">
+                <div className="container container-nav">
+                    <div>
+                        <h1 className=' titulo' >MoviesApp</h1>
+                    </div>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
 
-    <header>
-     
-
-     
-    </header>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <Link className='nav-link' to="/listado">Home</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className='nav-link' to="/favoritos">Favorites</Link>
+                            </li>
+                            <li className="nav-item d-flex align-items-center">
+                                <span className="text-secondary text">
+                                    {props.favorites.length > 0 && <>Movies in favorites: {props.favorites.length}</>}
+                                </span>
+                            </li>
+                        </ul>
+                    </div>
+                    <Search />
+                </div>
+            </nav>
+        </header>
     )
-
 }
 
 
