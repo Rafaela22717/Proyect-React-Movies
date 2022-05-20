cf35f47d3203d370eee305d61491985d02b9d690
Rafaela <rafaelaru22@hotmail.com>
2022-05-20 13:49:04 -0300

    Component Search finished

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,34 +2,44 @@ import React from 'react';
 import sweetAlert from '@sweetalert/with-react';
 import { useHistory } from "react-router-dom"
 
-function Search() {
-  const history = useHistory();
+function Search(props, movie) {
+    const history = useHistory();
 
     const handlerSubmit = e => {
         e.preventDefault();
         const keyword = e.currentTarget.keyword.value.trim();//capturo el formulario q disparo el evento//trim corta al principio y al final
         //validacion:
-        if (keyword.length ===0) {
-            sweetAlert(<h5>Ingresa un nombre</h5>)
-        } else if (keyword.length<3){
-            sweetAlert(<h5>Escribe tres o mas caracteres</h5>)
-        } else { 
+        if (keyword.length === 0) {
+            sweetAlert({
+                title: "Ingrese un nombre",
+                icon: "info",
+                button: false,
+                className: "sweet",
+                timer: 2500
+            });
+        } else if (keyword.length < 3) {
+            sweetAlert({
+                title: "Ingrese mas caracteres",
+                icon: "error",
+                button: false,
+                className: "sweetA",
+                timer: 2500
+            });
+        } else {
             e.currentTarget.keyword.value = "";//antes del push dejalo vacio
             history.push(`/resultados?keyword=${keyword}`);//redirecciono a resultados con la palabra clave
         }
     }
 
-
-
-
     return (
-        <form className='d-flex align-items-center' onSubmit={handlerSubmit}>
+        <form className="d-flex align-items-center" onSubmit={handlerSubmit}>
             <label className='form-label mb-0 mx-2'>
-                <input className='form-control' type="text" name="keyword" placeholder='Search..' />
+                <input className='form-control' type="text" name="keyword" placeholder='Enter movie..' />
             </label>
-            <button className='btn btn-success' type="submit">Buscar</button>
+            <button type="submit buttonS"
+                onClick={props.addOrRemoveFromFavs}
+                data-movie-id={movie.id}>Search</button>
         </form>
-
     )
 }
 
