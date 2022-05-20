a03e69baae4de9b6160d925963f6edb4ab88c088
Rafaela <rafaelaru22@hotmail.com>
2022-05-20 13:22:11 -0300

    Final modification Detail Component

--- src/components/Detalle.js
+++ src/components/Detalle.js
@@ -14,7 +14,7 @@ function Detalle() {
 
 
     useEffect(() => {
-        const endpoint = `https://api.themoviedb.org/3/movie/${movieID}?api_key=e0cd83b465cb791c8444c563ec079dbf&language=es-ES`
+        const endpoint = `https://api.themoviedb.org/3/movie/${movieID}?api_key=e0cd83b465cb791c8444c563ec079dbf`
         console.log(endpoint)
         axios.get(endpoint)
             .then(response => {
@@ -24,34 +24,30 @@ function Detalle() {
             .catch(error => {
                 console.log(error);
             })
-
     }, [movieID]);
-
-
 
 
 
     return (
         <>
             {!token && <Redirect to=" / " />}
-            {!movie && <p>Cargando...</p>}
+            {!movie && <p>Charging...</p>}
             {movie &&// si tengo info en movie renderiza//renderizado condicional
                 <>
-                    <h2 className="colorDetalle">Titulo: {movie.title}</h2>
+                    <h2 className="colorDetalle">Title: {movie.title}</h2>
                     <div className='row'>
                         <div className='col-4'>
                        
                             <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} className="img-fluid" alt="movie poster" />
                         </div>
                         <div className='col-8'>
-                            <h5 className="colorDetalle">Fecha de estreno: {movie.release_date}</h5>
-                            <h5 className="colorDetalle">Reseña:</h5>
+                            <h5 className="colorDetalle">Review:</h5>
                             <p className="colorDetalle">{movie.overview}</p>
 
                             <h5 className="colorDetalle">Rating: {movie.vote_average}</h5>
                             <h5 className="colorDetalle">Release date : {movie.release_date}</h5>
                             <h5 className="colorDetalle">Popularity: {movie.popularity}</h5>
-                            <h5 className="colorDetalle">Generos:</h5>
+                            <h5 className="colorDetalle">Genders:</h5>
                             <ul className="colorDetalle">
                                 {movie.genres.map(oneGenre => <li key={oneGenre.id}>{oneGenre.name}</li>)}
                             </ul>
@@ -59,10 +55,8 @@ function Detalle() {
                     </div>
                 </>
             }
-
         </>
     )
-
 }
 
 
